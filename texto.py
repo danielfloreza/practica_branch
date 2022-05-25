0d19746758346ab143cd0d89e0aa6e7d9473033b
Daniel Florez <danielflorezanalista@gmail.com>
2022-05-25 11:35:06 -0500

    Creo una interfaz sólo con raiz vacía y con título sencillo.

---
+++ texto.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+def run(root):
+    root.resizable(1, 1)
+    root.title("Practica de branch")
+
+
+if __name__ == '__main__':
+    root= Tk()
+    run(root)
+    root.mainloop()
