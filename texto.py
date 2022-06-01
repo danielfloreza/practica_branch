bc015ef9c80e3c9b3447991ec91bb4a0cc6e29e0
Daniel Florez <danielflorezanalista@gmail.com>
2022-06-01 10:38:16 -0500

    cambio el color en la misma linea pero a rojo para continuar con el conflicto

--- texto.py
+++ texto.py
@@ -10,7 +10,7 @@ def run(root):
     frame=Frame()
     frame.pack(fill="y", expand=1)
     frame.config(width="1000",height="450") 
-    frame.config(bg="white", bd=15, relief="groove", cursor="hand2")
+    frame.config(bg="red", bd=15, relief="groove", cursor="hand2")
 
 if __name__ == '__main__':
     root= Tk()
